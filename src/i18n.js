f2dedcd3b3f3903c23d421d1baf95481fd06e7b7
Sanchit <32457437+sanchit0496@users.noreply.github.com>
2025-03-01 14:12:16 +0530

    have created separate files for each lang on the app

--- src/i18n.js
+++ src/i18n.js
@@ -1,44 +1,18 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import {initReactI18next} from 'react-i18next'
+import Backend from 'i18next-http-backend'
 
 i18n.
     use(LanguageDetector)
     .use(initReactI18next)
+    .use(Backend)
     .init({
     lng: 'en', // if you're using a language detector, do not define the lng option
     debug: true,
     fallbackLng: 'en',
     returnObjects: true,
-    resources: {
-      en: {
-        translation: {
-          greeting: "Hello",
-          description:{
-            line1: 'eng',
-            line2: 'desc <1>{{test}}</1>'
-          } 
-        }
-      },
-      fr: {
-        translation: {
-          greeting: "Hola",
-          description:{
-            line1: 'fr',
-            line2: 'desc <1>{{test}}</1>'
-          } 
-        }
-      },
-      hi: {
-        translation: {
-          greeting: "Nmste",
-          description:{
-            line1: 'hi',
-            line2: 'desc <1>{{test}}</1>'
-          } ,
-        }
-      }
-    }
+
   });
 
 
